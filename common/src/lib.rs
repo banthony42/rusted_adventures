f37a81c5a4984c92c14962f949db1115add0cbc6
banthony42 <16167405+banthony42@users.noreply.github.com>
2025-10-16 13:36:22 +0200

    common: make some space between CHAT constants

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -42,10 +42,25 @@ pub mod constants {
     pub const GUI_WIDTH_CENTER: usize = (WINDOW_WIDTH - GUI_WIDTH) / 2;
     pub const GAME_HEIGHT: usize = MAP_HEIGHT + GUI_HEIGHT;
 
-    pub const GUI_CHAT_X: usize = 20;
-    pub const GUI_CHAT_Y: usize = MAP_HEIGHT + 18;
-    pub const GUI_CHAT_WIDTH: usize = 408;
-    pub const GUI_CHAT_HEIGHT: usize = 140;
+    pub const CHAT_FONT_SIZE: u32 = 17;
+
+    pub const GUI_CHAT_X: f64 = 16.0;
+    pub const GUI_CHAT_Y: f64 = MAP_HEIGHT as f64 + 18.0;
+    pub const GUI_CHAT_WIDTH: f64 = 416.0;
+    pub const GUI_CHAT_HEIGHT: f64 = 140.0;
+    pub const GUI_CHAT_SIZE: [f64; 4] = [GUI_CHAT_X, GUI_CHAT_Y, GUI_CHAT_WIDTH, GUI_CHAT_HEIGHT];
+    pub const GUI_CHAT_PADDING_WIDTH: f64 = 5.0;
+
+    pub const CHAT_INPUT_POSITION: [f64; 2] = [16.0, 928.0];
+    pub const CHAT_INPUT_WIDTH: f64 = GUI_CHAT_WIDTH;
+
+    pub const CHAT_WINDOW_TIMER: u128 = 8000;
+    pub const CHAT_WINDOW_WIDTH: f64 = 128.0;
+    pub const CHAT_WINDOW_PADDING_HEIGHT: f64 = 10.0;
+    pub const CHAT_WINDOW_PADDING_WIDTH: f64 = GUI_CHAT_PADDING_WIDTH;
+    pub const CHAT_WINDOW_MARGIN_BOTTOM: f64 = 5.0;
+
+    pub const GUI_ENTITY_FONT_SIZE: u32 = 17;
 
     // Window size
     pub const WINDOW_WIDTH: usize = MAP_WIDTH;
